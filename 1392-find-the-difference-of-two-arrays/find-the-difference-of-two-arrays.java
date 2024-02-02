5e0f040f9edd77c0447969af7a72e38b5d3ba4f7
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-02 23:50:58 +1100

    Time: 9 ms (93.66%) | Memory: 45.6 MB (21.73%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -12,22 +12,26 @@ class Solution
         //result list containing nums1Distinct and nums2Distinct
         List<List<Integer>> result = new ArrayList<>();
 
-        //Hash Set to store the non-duplicate key
+        //Hash Set to store the non-duplicate keys in nums1
         Set<Integer> set1 = new HashSet<>();
+
+        //Hash Set to store the non-duplicate keys in nums2
         Set<Integer> set2 = new HashSet<>();
 
+        //populating set1
         for ( int num: nums1)
         {
                 set1.add(num);
         }
 
-        
+        //populating set2
         for ( int num: nums2)
         {
             set2.add(num);
         }
 
-       for ( int num: set1)
+        //populating nums1Distinct with non-duplicate values in nums1
+        for ( int num: set1)
         {
             if (!set2.contains(num))
             {
@@ -35,6 +39,7 @@ class Solution
             }
         }
 
+        //populating nums2Distinct with non-duplicate values in nums2
         for ( int num: set2)
         {
             if (!set1.contains(num))
@@ -43,10 +48,11 @@ class Solution
             }
         }
         
-
+        //adding two final lists
         result.add(nums1Distinct);
         result.add(nums2Distinct);
-        //returning the value
+
+        //returning resultant list of the list
         return result;
     }
 }
