ddf56750c0013bcf9ae1baaf731c2e45c3c02e81
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-12 10:25:09 +1100

    Time: 5 ms (41.19%) | Memory: 55.9 MB (46.18%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,16 +1,15 @@
 class Solution 
 {
 
+    //the main idea is to use sliding window technique
     public double findMaxAverage(int[] nums, int k) 
     {
-       
         double maxAverage =0.0;
-
         double maxSum = 0.0;
         double currentSum = 0.0;
         
 
-        //find the sum of first k elements
+        //find the sum of first k elements and assign masSum as the sum of these k elements
         for (int i=0; i< k; i++)
         {
             currentSum = currentSum + nums[i];
@@ -18,12 +17,13 @@ class Solution
         }
         maxSum = currentSum;
 
-        int count = 0; 
+        //variable for pointing to the start of the window
+        int startIndx = 0; 
 
         for ( int i= k; i < nums.length; i++ )
         {
-            currentSum = currentSum - nums[count] + nums[i];
-            count ++;
+            currentSum = currentSum - nums[startIndx] + nums[i];
+            startIndx ++;
             maxSum = Math.max(currentSum,maxSum);
         }
 
