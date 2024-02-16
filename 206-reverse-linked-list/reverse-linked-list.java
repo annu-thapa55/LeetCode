a8932e75381798aad649016a583584774fe19662
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-16 12:14:18 +1100

    Time: 0 ms (100.00%) | Memory: 42.8 MB (14.66%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -12,22 +12,29 @@ class Solution
 {
     public ListNode reverseList(ListNode head) 
     {
-
+        //returning head if the head is null
         if (head == null)
             return head;
-            
+        
+        //assigning values to current, previous and next ListNode variables.
         ListNode current = head;
         ListNode previous = null;
         ListNode next = null;
 
+        //traversing until the end of the linked list where two nodes are operated in each iteration to reverse
         while (current!=null)
         {
+            //next points to the second node in reverse operation
             next = current.next;
+            //performing reverse operation
             current.next = previous;
+            //pointing to the first node in reverse operation
             previous = current;
+            // traversing by one node to perform reverse operation in next iteration
             current = next;
         }
     
+        // returning previous as it points to the last node of the input linked list
         return previous; 
     }
 }
