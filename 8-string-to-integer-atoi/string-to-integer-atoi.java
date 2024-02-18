f2124960399dd3ddaf643f4b4576c12226c828c2
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-18 21:24:27 +1100

    Time: 1 ms (100.00%) | Memory: 42.2 MB (71.25%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -2,7 +2,9 @@ class Solution
 {
     public int myAtoi(String s) 
     {
-        //Taking care of corner cases
+        //First take care of corner cases:
+
+        //taking care of input string length constraints
         if(s.length() <= 0 || s.length() >= 200)
         {
             return 0;
@@ -11,25 +13,28 @@ class Solution
         //convert the string into character array
         char[] charArr = s.toCharArray();
         
+        //extracted int value to be returned
         int result = 0;
         
+        //boolean value to indicate if the input string is positive or negative
         boolean positive =true;
-        
+
+        //index for traversing the character array made from input string
         int count = 0;
         
-        //white space check
+        //taking care of white spaces
         while (count < charArr.length && charArr[count] ==' '  )
         {
             count++;
         }
 
+        //returning 0 for the string with only white spaces
         if(count >= charArr.length)
         {
             return 0;
         }
         
-        //take care of +, - sign
-        
+        //taking care of +, -, or none symbol after spaces
         if(count < charArr.length && charArr[count] == '-')
         {
             positive = false; 
@@ -42,46 +47,57 @@ class Solution
             count++;
         }
 
-        //for 0s in the beginning
+        //taking care of 0s after the +, - or spaces
         while (count < charArr.length && charArr[count] == 0)
         {
             count++;
         }
         
-        // for characters
+        //starting the digit extraction
       
+        //variable for traversing the character array and extracting the digits
         int digitCount = count;
+
+        //variable for identifying the first digit
         int firstDigit = count; 
         while (digitCount < charArr.length && charArr[digitCount] >= '0' && charArr[digitCount] <= '9')
         {
+            //extracting the digit from character array
             int digit = Character.getNumericValue(charArr[digitCount]); 
 
+            //calculating result for the first digit
             if(digitCount == firstDigit)
             {
                 result = digit;  
             }
+
+            //calculating result for non-first exracted digits
             else
             {
+                //checking the stack overflow condition before multiplying previous result by 10 and adding current extracted digit
                 if ((result > Integer.MAX_VALUE / 10) || 
                 (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10))
                 {
                     if(positive)
                       
-
                         return Integer.MAX_VALUE ;
                     else
                         return Integer.MIN_VALUE;
                 }
+                
+                // calculating result in non-overflow condition
                 else
                 {
                     result = result *10 + digit;
                 }
             }
 
+            // incrementing the traversing index by 1
             digitCount++;
         }
       
         
+        // returning result in terms of + or - sign in input string
         if(positive)
         {
             return result;
