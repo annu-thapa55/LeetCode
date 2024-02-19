5148412a2fb4047ef7895df5f6c6ce6fc08bf6dc
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-20 10:24:50 +1100

    Time: 5 ms (72.92%) | Memory: 58.4 MB (11.99%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -2,16 +2,9 @@ class Solution
 {
     public int maxArea(int[] height) 
     {
-        //corner cases
         
-        if ( height.length< 2 || height.length > Math.pow(10,5))
-        {
-            return 0;
-        }
         //max area
         int maxArea =0;
-        
-        int currentArea = 0;
         
         //two pointers to iterate height array. One at the beginning of array and another at end of array.
         
@@ -19,22 +12,12 @@ class Solution
         
         int endIndex = height.length -1;
         
-        int tankLength = height.length -1; 
         
         //iterate through height array
         
         while (startIndex < endIndex)
         {
-          
-            if(height[startIndex] <0 || height[endIndex] <0|| height[startIndex]> Math.pow(10,4) ||         height[endIndex] > Math.pow(10,4))
-            {
-                return 0; 
-            }
-            
-            currentArea = Math.min(height[startIndex],height[endIndex]) * tankLength;
-            maxArea = Math.max(currentArea, maxArea);
-            
-            tankLength --;
+            maxArea = Math.max((Math.min(height[startIndex],height[endIndex]) * (endIndex-startIndex)), maxArea);
             
             if(height[startIndex] < height[endIndex] )
             {
