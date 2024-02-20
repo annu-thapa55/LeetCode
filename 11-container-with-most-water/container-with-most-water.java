62ada4da5cc6c53949c1aaa7b1fcb7bdd0f42f85
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-20 11:51:34 +1100

    Time: 2 ms (99.38%) | Memory: 57.9 MB (57.60%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -14,16 +14,20 @@ class Solution
         
         
         //iterate through height array
-        
         while (startIndex < endIndex)
         {
+            //minimum height pointed by startIndex, and endIndex as the minimum height between two heights is taken into consideration for water area
             int minHeight = Math.min(height[startIndex],height[endIndex]);
+
+            //max area selected between current area and maxArea so far
             maxArea = Math.max((minHeight* (endIndex-startIndex)), maxArea);
             
+            //iterate startIndex until the height pointed by it is smaller than the minHeight because smaller height won't contribute to the maxArea. 
             while(startIndex < endIndex && height[startIndex] <= minHeight)
             {
                 startIndex++;
             }
+            //iterate endIndex for the same reason
             while (startIndex < endIndex && height[endIndex] <= minHeight)
             {
                 endIndex--;
