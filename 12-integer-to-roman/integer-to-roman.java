2fb8d680923e6317f2825c779cf4d0f6abf18b28
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-20 15:09:08 +1100

    Time: 2 ms (100.00%) | Memory: 44.2 MB (72.76%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -19,11 +19,10 @@ class Solution
         {
             //keep on subtracting the currently pointed value from the number and add the corresponding symbol
             while(arrValue[index] <= num)
-            {   
+            {  
+                result.append(arrSymbol[index]);   
                 num -= arrValue[index];     
-                result.append(arrSymbol[index]);  
-               
-                
+                             
             }
         }
         //return result        
