a817d126a6cda2cbe31904cfae4f80d186749cac
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-21 16:05:51 +1100

    Time: 6 ms (38.69%) | Memory: 44.6 MB (62.18%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,42 +1,23 @@
-class Solution {
+class Solution 
+{
+    final static String[] arrSymbol = {"M", "CM", "D","CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    final static int[] arrValue = {1000, 900, 500,400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     
-    static Map<String, Integer> values = new HashMap<>();
-
-    static {
-        values.put("I", 1);
-        values.put("V", 5);
-        values.put("X", 10);
-        values.put("L", 50);
-        values.put("C", 100);
-        values.put("D", 500);
-        values.put("M", 1000);
-        values.put("IV", 4);
-        values.put("IX", 9);
-        values.put("XL", 40);
-        values.put("XC", 90);
-        values.put("CD", 400);
-        values.put("CM", 900);
-    }
-
-    public int romanToInt(String s) {
-        
-        int sum = 0;
-        int i = 0;
-        while (i < s.length()) {
-            if (i < s.length() - 1) {
-                String doubleSymbol = s.substring(i, i + 2);
-                // Check if this is the length-2 symbol case.
-                if (values.containsKey(doubleSymbol)) {
-                    sum += values.get(doubleSymbol);
-                    i += 2;
-                    continue;
-                }
+    public int romanToInt(String s) 
+    {
+        int result = 0;
+        StringBuilder inputStr = new StringBuilder(s);
+        int symbolLength = 0;
+        for(int i=0; i<arrSymbol.length && inputStr.length() > 0; i++)
+        {
+            symbolLength = arrSymbol[i].length();
+            while(inputStr.length()>= symbolLength && inputStr.substring(0,symbolLength).equals(arrSymbol[i]))
+            {
+                result = result + arrValue[i];
+                inputStr = inputStr.replace(0, symbolLength, "");
+                
             }
-            // Otherwise, it must be the length-1 symbol case.
-            String singleSymbol = s.substring(i, i + 1);
-            sum += values.get(singleSymbol);
-            i += 1;
         }
-        return sum;
+        return result;
     }
 }
