4406c8b6053df08356418c17025a819bcdb00d87
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-21 15:35:49 +1100

    Time: 151 ms (5.55%) | Memory: 45.2 MB (9.07%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,26 @@
+class Solution 
+{
+    final static String[] arrSymbol = {"M", "CM", "D","CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    final static int[] arrValue = {1000, 900, 500,400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    
+    public int romanToInt(String s) 
+    {
+        int result = 0;
+        StringBuilder inputStr = new StringBuilder(s);
+        int symbolLength = 0;
+        for(int i=0; i<arrSymbol.length && inputStr.length() > 0; i++)
+        {
+            symbolLength = arrSymbol[i].length();
+            while(inputStr.length()>= symbolLength && inputStr.substring(0,symbolLength).equals(arrSymbol[i]))
+            {
+                System.out.println("inputStr value before="+inputStr);
+                result = result + arrValue[i];
+                System.out.println("result="+result);
+                inputStr = inputStr.replace(0, symbolLength, "");
+                System.out.println("inputStr value after="+inputStr);
+               
+            }
+        }
+        return result;
+    }
+}
