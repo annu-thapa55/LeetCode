ef0b93461f4db1f943f4c28a291172c34bb0f3cd
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-22 14:45:35 +1100

    Time: 518 ms (23.48%) | Memory: 52 MB (32.04%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,20 +1,99 @@
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
-        Set<Integer> dups = new HashSet<>();
-        Map<Integer, Integer> seen = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i)
-            if (dups.add(nums[i])) {
-                for (int j = i + 1; j < nums.length; ++j) {
-                    int complement = -nums[i] - nums[j];
-                    if (seen.containsKey(complement) && seen.get(complement) == i) {
-                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
-                        Collections.sort(triplet);
-                        res.add(triplet);
-                    }
-                    seen.put(nums[j], i);
-                }
+
+class Solution 
+{
+
+    public List<List<Integer>> threeSum(int[] nums) 
+    {
+        List<List<Integer>> result = new ArrayList<>();
+        Set<Integer> firstNum = new HashSet<>();
+        Set<Integer> secondNum = new HashSet<>();
+        Set<Integer> thirdNum = new HashSet<>();
+
+        int j =0;
+        int k=0;
+        int complementRes = 0;
+
+        Arrays.sort(nums);
+            
+        for(int i =0; i<nums.length; i++)
+        {
+            if(firstNum.contains(nums[i]))
+            {
+                continue;
             }
-        return new ArrayList(res);
+            else
+            {
+                firstNum.add(nums[i]);
+                j= i+1;
+                k= nums.length -1;
+
+                secondNum.clear();
+                thirdNum.clear();
+
+                boolean secondNumSeen = false;
+                boolean thirdNumSeen = false; 
+
+                while(j<k)
+                {
+                   
+ 
+                    if(secondNum.contains(nums[j]) && secondNumSeen)
+                    {
+       
+                        j++;
+                        continue;
+                    }
+
+                    if(thirdNum.contains(nums[k]) && thirdNumSeen)
+                    {
+                       
+                        k--;
+                       
+                        continue;
+                    }
+                   
+                 
+                  
+                    secondNum.add(nums[j]);
+                    secondNumSeen = false;
+                    thirdNum.add(nums[k]);
+                    thirdNumSeen = false;
+
+                    complementRes = -nums[j] -nums[k];
+                
+                    if(complementRes == nums[i])
+                    {
+                        List<Integer> currentRes = new ArrayList<Integer>(); 
+                        currentRes.add(nums[i]);
+                        currentRes.add(nums[j]);
+                        currentRes.add(nums[k]);
+
+
+                        result.add(currentRes);
+                        j++;
+                        k--;
+                        secondNumSeen = true;
+                        thirdNumSeen =true;
+                    }
+
+                    else if (complementRes > nums[i])
+                    {
+                        j++;
+                        secondNumSeen = true;
+                    }
+                    else
+                    {
+                        k--;
+                        thirdNumSeen =true;
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        //return list of combination
+        return result;
+
     }
 }
