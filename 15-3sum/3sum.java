b2661c5947a1a0f8bdd31a7af16ca11a24345ff1
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-22 14:41:31 +1100

    Time: 290 ms (30.32%) | Memory: 51.6 MB (52.59%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,99 +1,23 @@
-
-class Solution 
-{
-
-    public List<List<Integer>> threeSum(int[] nums) 
-    {
-        List<List<Integer>> result = new ArrayList<>();
-        Set<Integer> firstNum = new HashSet<>();
-        Set<Integer> secondNum = new HashSet<>();
-        Set<Integer> thirdNum = new HashSet<>();
-
-        int j =0;
-        int k=0;
-        int complementRes = 0;
-
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-            
-        for(int i =0; i<nums.length; i++)
-        {
-            if(firstNum.contains(nums[i]))
-            {
-                continue;
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; ++i)
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSum(nums, i, res);
             }
-            else
-            {
-                firstNum.add(nums[i]);
-                j= i+1;
-                k= nums.length -1;
-
-                secondNum.clear();
-                thirdNum.clear();
-
-                boolean secondNumSeen = false;
-                boolean thirdNumSeen = false; 
-
-                while(j<k)
-                {
-                   
- 
-                    if(secondNum.contains(nums[j]) && secondNumSeen)
-                    {
-       
-                        j++;
-                        continue;
-                    }
-
-                    if(thirdNum.contains(nums[k]) && thirdNumSeen)
-                    {
-                       
-                        k--;
-                       
-                        continue;
-                    }
-                   
-                 
-                  
-                    secondNum.add(nums[j]);
-                    secondNumSeen = false;
-                    thirdNum.add(nums[k]);
-                    thirdNumSeen = false;
-
-                    complementRes = -nums[j] -nums[k];
-                
-                    if(complementRes == nums[i])
-                    {
-                        List<Integer> currentRes = new ArrayList<Integer>(); 
-                        currentRes.add(nums[i]);
-                        currentRes.add(nums[j]);
-                        currentRes.add(nums[k]);
-
-
-                        result.add(currentRes);
-                        j++;
-                        k--;
-                        secondNumSeen = true;
-                        thirdNumSeen =true;
-                    }
-
-                    else if (complementRes > nums[i])
-                    {
-                        j++;
-                        secondNumSeen = true;
-                    }
-                    else
-                    {
-                        k--;
-                        thirdNumSeen =true;
-                    }
-                    
-                }
-                
+        return res;
+    }
+    void twoSum(int[] nums, int i, List<List<Integer>> res) {
+        var seen = new HashSet<Integer>();
+        for (int j = i + 1; j < nums.length; ++j) {
+            int complement = -nums[i] - nums[j];
+            if (seen.contains(complement)) {
+                res.add(Arrays.asList(nums[i], nums[j], complement));
+                while (j + 1 < nums.length && nums[j] == nums[j + 1])
+                    ++j;
             }
-            
+            seen.add(nums[j]);
         }
-        //return list of combination
-        return result;
-
     }
 }
