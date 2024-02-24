b205c54c5dd6d70adeed7d6d9b06c494d253155e
annu-thapa55 <127836500+annu-thapa55@users.noreply.github.com>
2024-02-24 14:10:21 +1100

    Time: 0 ms (100.00%) | Memory: 42.2 MB (76.75%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -12,72 +12,46 @@ class Solution
 {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2)
     {
-     
+        // creating a dummy node as a new node.  
+        ListNode result = new ListNode(0);
+        //temporary node pointing to newly created dummy node to perform weaving
+        ListNode temp = result;
 
 
-        //corner cases
-
-
-        if(list1 == null && list2 == null)
+        //condition of the loop should be that neither of the two nodes pointed by list1 and list2 should be null. 
+        //condition of one list being exhausted first than another will be taken care after the loop
+        while (list1!=null && list2 !=null)
         {
-            return null;
-        }
-
-
-        else if(list1 == null && list2 != null)
-        {
-            return list2;
-        }
-       
-        else if(list1 != null && list2 == null)
-        {
-            return list1;
-        }
-       
-        else
-        {
-        
-            //temporary node to perform weaving
-            ListNode result = new ListNode(0);
-            ListNode temp = result;
-
-
-            //loop to compare and weave
-            while (list1!=null && list2 !=null)
-            {
-               
-                if (list1.val <= list2.val)
-                {
-                    temp.next = list1;
-                    list1 = list1.next;
-                }
-                else
-                {
-                    temp.next = list2;
-                    list2 = list2.next;
-                }
-                temp = temp.next;
-
-            }
-
-
-            // if list1 is not exhausted
-            if(list1 != null)
+            //weave lesser value node to the temp and advance weaved list by one node
+            if (list1.val <= list2.val)
             {
                 temp.next = list1;
+                list1 = list1.next;
             }
             else
             {
                 temp.next = list2;
+                list2 = list2.next;
             }
+            //advance temp by one node after weaving
+            temp = temp.next;
 
-
-             //return the merged list
-            return result.next;
         }
-       
+
+        // if list1 is not exhausted
+        if(list1 != null)
+        {
+            temp.next = list1;
+        }
+        // if list2 is not exhausted
+        else 
+        {
+            temp.next = list2;
+        }
 
 
+        //return the head of the merged list as the head is pointed by the result.next after excluding the dummy node. 
+        return result.next;
        
     }
 }
